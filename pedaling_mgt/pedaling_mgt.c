0d214503bfb623465ea3e0fb4e7bf1b8a6e8dbe0
nicolas.cassar <nicolas.cassar@outlook.fr>
2024-05-24 13:38:08 +0200

    get_average_pedaling_speed implementation

--- pedaling_mgt/pedaling_mgt.c
+++ pedaling_mgt/pedaling_mgt.c
@@ -9,6 +9,7 @@
 /**** Defines ****/
 
 #define MAGNET_NBR 12 /// Number of magnets on pedaling sensor (no unit)
+#define US_IN_A_MINUTE 60000000.f /// number of microseconds in one minute (us)
 
 /**** Private variables ****/
 
@@ -25,8 +26,11 @@ float get_pedaling_speed(void)
 
 float get_average_pedaling_speed(void)
 {
-    // WRITE YOUR CODE HERE
-    return 0.f;
+    float average_pedaling_speed = 0.0;
+    /// average_pedaling_speed = 1min / (actualTimestamp - oldestTimestamp)
+    average_pedaling_speed = US_IN_A_MINUTE / (magnet_timestamp_array[index] - magnet_timestamp_array[(index + 1) % MAGNET_NBR]);
+
+    return average_pedaling_speed;
 }
 
 void new_magnet_cb(void)
